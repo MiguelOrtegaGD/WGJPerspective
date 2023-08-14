b40348bb13e2102617b82238c3a02c828f1d9ba3
Miguel-UGameStudio <migelortez@hotmail.com>
2023-08-14 00:46:46 -0500

    Sound

---
+++ Assets/Scripts/AudioEventListener.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEventListener : MonoBehaviour
+{
+    [SerializeField] List<string> soundEffects = new List<string>();
+    public void PlaySoundEffect()
+    {
+        int random = Random.Range(0, soundEffects.Count);
+        AudioManager.Instance.PlayMainSfx(soundEffects[random]);
+    }
+}
